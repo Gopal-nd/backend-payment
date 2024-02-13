813a066a5ffa9aeefa2ca1575ce4d83d0d34df0b
Gopal N D <gopalhelpyou@gmail.com>
2024-02-13 22:08:30 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+
 
 
 const port = 3001;
